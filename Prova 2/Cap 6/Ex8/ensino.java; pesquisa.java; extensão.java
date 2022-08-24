b71d2daf0e91cf9bae0329d387afd3706381d463
EdYashida <107697323+EdYashida@users.noreply.github.com>
2022-08-24 10:38:36 -0300

    Create ensino.java; pesquisa.java; extensão.java

---
+++ Prova 2/Cap 6/Ex8/ensino.java; pesquisa.java; extensão.java
@@ -0,0 +1,15 @@
+public class Ensino extends Projeto{
+    String disciplina;
+    String curso;
+}
+
+public class Pesquisa extends Projeto {
+    String areaCNPQ;
+    Double orcamento;
+    
+}
+
+public class Extensao extends Projeto{
+    String publicoAlvo;
+    String linha;
+}
