91587e4b2dccd914f3c3da039c076003eefd60e2
EdYashida <107697323+EdYashida@users.noreply.github.com>
2022-08-24 10:37:32 -0300

    Update and rename Aoba to Projeto.java

---
+++ Prova 2/Cap 6/Ex8/Projeto.java
@@ -0,0 +1,18 @@
+public abstract class Projeto {
+    private int numero;
+    private String nome;
+    
+    public int getNum(){
+        return this.numero;
+    }
+    public void setNum(int n){
+        this.numero=n;
+    }
+    
+    public String getNome(){
+        return this.nome;
+    }
+    public void setNome(String no){
+        this.nome=no;
+    }
+}
